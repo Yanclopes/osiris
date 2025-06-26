6f35140353e257b2fb9f022bb53788daad51eb40
cebola <cristhianmac123@gmail.com>
2025-06-25 23:50:57 -0300

    Atualizacao

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -9,6 +9,16 @@ interface AppLayoutProps {
 
 export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
     <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
-        {children}
+        <div className="relative min-h-screen">
+            <img
+                src="/watermark.svg"
+                alt="Marca d'água"
+                className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-10 pointer-events-none select-none z-0"
+                style={{ width: '40vw', maxWidth: 500 }}
+            />
+            <div className="relative z-10">
+                {children}
+            </div>
+        </div>
     </AppLayoutTemplate>
 );
